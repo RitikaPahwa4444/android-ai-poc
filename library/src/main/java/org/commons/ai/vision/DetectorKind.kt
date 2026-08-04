d3aa1aa4c30bd274b6fb55e43d36275c733eaead
Ritika Pahwa <ritikapahwa4444@gmail.com>
2026-08-05 00:03:05 +0530

    Revert "Make detector metadata configuration driven"
    This reverts commit 99a1a52174e1f1e40fff2ff760b14646340eab6a.

--- library/src/main/java/org/commons/ai/vision/DetectorKind.kt
+++ library/src/main/java/org/commons/ai/vision/DetectorKind.kt
@@ -3,15 +3,16 @@ package org.commons.ai.vision
 import org.commons.ai.common.DetectionType
 
 internal enum class DetectorKind(
-    val config: DetectorConfig
-) {
-    FACE(DetectorConfig("models/face_detection_yunet_2023mar.ort", 320, 320, DetectionType.FACE)),
-    LICENSE_PLATE(DetectorConfig("models/license_plate_detection_lpd_yunet_2023mar_int8.ort", 320, 240, DetectionType.LICENSE_PLATE))
-}
-
-internal data class DetectorConfig(
     val assetName: String,
     val inputWidth: Int,
     val inputHeight: Int,
     val detectionType: DetectionType
-)
+) {
+    FACE("models/face_detection_yunet_2023mar.ort", 320, 320, DetectionType.FACE),
+    LICENSE_PLATE(
+        "models/license_plate_detection_lpd_yunet_2023mar_int8.ort",
+        320,
+        240,
+        DetectionType.LICENSE_PLATE
+    )
+}
